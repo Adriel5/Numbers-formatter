bff941611b1ca5411afdbfc020705feda45e78c4
Adriel V. Raupp <88636474+Adriel5@users.noreply.github.com>
2023-12-29 12:23:18 -0300

    Update functions.js
    Optimized CEP and added FormatFone

--- functions.js
+++ functions.js
@@ -1,20 +1,27 @@
-    var cepV = 0
-    function formatCEP(cep) {
+function formatCEP(cep) {
         cep = cep.toString().replace(/[^0-9]/g, '')
-        if (cep.length > 5) {
-            cep = cep.slice(0, 5) + '-' + cep.slice(5)
-        } if (cep.length == 6) {
-            if (cepV == 0) {
-                cep = cep.slice(0, 5) + '-' + cep.slice(5)
-                cepV = 1
-            } else if (cepV == 1) {
-                cep = cep.replace('-', '')
-                cepV = 0
-            }
-        } else if (cep.length > 9) {
-            cep = cep.slice(0, 9).replace('-', '')
+        if(cep.length>5){
             cep = cep.slice(0, 5) + '-' + cep.slice(5)
         }
-    
+        if(cep.length>9){
+            cep = cep.slice(0, 9)
+        }
         return cep
+    }
+
+    function formatFone(fone) {
+        fone = fone.toString().replace(/[^0-9]/g, '')
+        if(fone.length>0){
+            fone = '(' +fone
+        }
+        if(fone.length>3){
+            fone = fone.slice(0, 3) + ') ' + fone.slice(3)
+        }
+        if(fone.length>10){
+            fone = fone.slice(0, 10) + '-' + fone.slice(10)
+        }
+        if(fone.length>15){
+            fone = fone.slice(0, 15)
+        }
+        return fone
     }
