4828d8b4f396340f992d879348466a386164bf08
Adriel V. Raupp <88636474+Adriel5@users.noreply.github.com>
2023-12-29 14:22:50 -0300

    Mais funções

--- functions.js
+++ functions.js
@@ -25,3 +25,48 @@ function formatCEP(cep) {
         }
         return fone
     }
+
+    function formatCPF(cpf) {
+        cpf = cpf.toString().replace(/[^0-9]/g, '')
+        if(cpf.length>3){
+            cpf = cpf.slice(0,3) + '.' + cpf.slice(3)
+        }
+        if(cpf.length>7){
+            cpf = cpf.slice(0,7)+'.' + cpf.slice(7)
+        }
+        if(cpf.length>11){
+            cpf = cpf.slice(0,11) + '-' +cpf.slice(11)
+        }
+        if(cpf.length>14){
+            cpf = cpf.slice(0, 14)
+        }
+        return cpf
+    }
+
+    function formatCNPJ(cnpj) {
+        cnpj = cnpj.toString().replace(/[^0-9]/g, '')
+        if(cnpj.length>2){
+            cnpj = cnpj.slice(0,2) + '.' + cnpj.slice(2)
+        }
+        if(cnpj.length>6){
+            cnpj = cnpj.slice(0, 6)+'.'+cnpj.slice(6)
+        }
+        if(cnpj.length>10){
+            cnpj = cnpj.slice(0, 10) + '/' + cnpj.slice(10)
+        }
+        if(cnpj.length>15){
+            cnpj = cnpj.slice(0, 15) + '-' +cnpj.slice(15)
+        }
+        if(cnpj.length>18){
+            cnpj = cnpj.slice(0, 18)
+        }
+        return cnpj
+    }
+
+    function formatRG(rg){
+        rg = rg.toString().replace(/[^0-9]/g, '')
+        if(rg.length>10){
+            rg = rg.slice(0,10)
+        }
+        return rg
+    }
